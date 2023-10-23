116ac8d60333b4cb1b220aa9f496675d1e0835c2
chanhwiKang <cksgnl4545@gmail.com>
2023-10-23 11:45:36 +0900

    10.23

--- INHA_1-2/GO/go/week09/main.go
+++ INHA_1-2/GO/go/week09/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	// pointer : 메모리 주소를 값으로 가지는 타입.
 	var a int = 10
 	var pa *int
 	pa = &a
 
 	fmt.Println(&a, a)
 	fmt.Println(pa, *pa)
+	fmt.Println(&pa)
+	fmt.Printf("%T %T", a, pa)
 }
