7c814b54fa9aece26694a3e4461af13113854811
chanhwiKang <cksgnl4545@gmail.com>
2023-11-13 10:43:23 +0900

    11/13

--- INHA_1-2/GO/go/week12/main.go
+++ INHA_1-2/GO/go/week12/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	// 방법 1
-	var s []int
-	s = make([]int, 5)
+	// var s []int
+	// s = make([]int, 5)
 
 	// 방법 2
+	// s := make([]int, 5)
+
+	// 방법 3
+	s := []int{0, 0, 0, 0, 0}
+
 	for _, v := range s {
 		fmt.Println(v)
 	}
