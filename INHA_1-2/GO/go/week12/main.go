60672f53cba6efc6f76d3b5e12aa0d595bdc9017
chanhwiKang <cksgnl4545@gmail.com>
2023-11-13 10:55:51 +0900

    "11/13"

--- INHA_1-2/GO/go/week12/main.go
+++ INHA_1-2/GO/go/week12/main.go
@@ -10,10 +10,21 @@ func main() {
 	// 방법 2
 	// s := make([]int, 5)
 
-	// 방법 3
-	s := []int{0, 0, 0, 0, 0}
+	// 방법 3 slice literal
+	s := []int{0, 1, 2, 3, 4}
+
+	s[4] = 99
 
 	for _, v := range s {
 		fmt.Println(v)
+	}
+
+	copyS := s[1:4]
+	for _, v := range copyS {
+		fmt.Println(v)
+	}
+
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
 	}
 }
