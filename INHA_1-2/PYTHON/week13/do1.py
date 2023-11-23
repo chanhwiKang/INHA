08a0fc9f72a519bef113f0a7f43855650bf0ad6c
chanhwiKang <cksgnl4545@gmail.com>
2023-11-23 17:26:07 +0900

    "파일 정리"

---
+++ INHA_1-2/PYTHON/week13/do1.py
@@ -0,0 +1,11 @@
+# # 방법 1
+# from student import Student1
+
+# print("step1", "-" * 10)
+# stu1 = Student1()
+
+# 방법 2
+import student as st
+
+print("step1", "-" * 10)
+stu1 = st.Student1()
---
+++ INHA_1-2/PYTHON/week13/student.py
@@ -0,0 +1,4 @@
+# 학생 정보를 관리하는 class
+
+class Student1:
+    pass
